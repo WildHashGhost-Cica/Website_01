c53336190ca5cce0526811c43ad2de91372bc4a0
WildHashGhost-Cica <wildhashghost@gmail.com>
2021-11-26 17:56:38 +0000

    Header & Navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,113 @@
-// This is Header component /Navigation Component
+import styled from 'styled-components';
+import logo from '../../assets/whg.jpg';
+
+const Headers = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 5rem;
+  background-color: var(--nav);
+  color: var(--white);
+  position: relative;
+  z-index: 500;
+  @media only Screen and (max-width: 64em) {
+    padding: 0.5rem 3rem;
+  }
+  @media only Screen and (max-width: 40em) {
+    padding: 0.5rem 1.5rem;
+  }
+`;
+
+const Logo = styled.a`
+  display: flex;
+  align-items: center;
+  width: 3rem;
+  height: auto;
+  cursor: pointer;
+  img {
+    margin-right: 0.5rem;
+  }
+`;
+
+const Nav = styled.nav`
+  width: 25rem;
+  max-width: 40rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  transition: all 0.3s;
+  @media only Screen and (max-width: 48em) {
+    display: none;
+  }
+  a {
+    font-weight: 600;
+    line-height: 1.5;
+    color: var(--white);
+    &::after {
+      content: "";
+      display: block;
+      height: 3px;
+      width: 0;
+      background: transparent;
+      transition: width 0.5s;
+    }
+    &:not(:last-child):hover::after {
+      width: 100%;
+      background: var(--purple);
+    }
+    /* &:not(:last-child) {
+      margin-right: 2rem;
+    } */
+    /* @media only Screen and (max-width: 48em) {
+      &:not(:last-child) {
+        margin-right: 1rem;
+      }
+    } */
+  }
+`;
+
+const Button = styled.button`
+  background-color: var(--purple);
+  padding: 0.5rem 1rem;
+  border-radius: 20px;
+  color: var(--white);
+  font-weight: 600;
+  cursor: pointer;
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:focus {
+    transform: scale(0.9);
+  }
+  @media only Screen and (max-width: 40em) {
+    font-size: 1.2rem;
+    &:hover {
+      transform: none;
+    }
+    &:focus {
+      transform: none;
+    }
+  }
+`;
 
 const Header = () => {
-  return <div>Header Component</div>;
+  return (
+  <Headers>
+  <Logo>
+    <img src={logo} alt="WHG"/>
+    <h3>WildHahsGhost</h3>
+  </Logo>
+  <Nav>
+    <a href="#home">Home</a>
+    <a href="#about">About Us</a>
+    <a href="#services">Services</a>
+    <a href="#contact">
+      <Button>Contact Us</Button>
+    </a>
+  </Nav>
+  </Headers>
+  );
 };
 
 export default Header;
