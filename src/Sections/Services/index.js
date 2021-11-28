fb04687b35ecbba0b3295214a7af75b0561d1d83
WildHashGhost-Cica <wildhashghost@gmail.com>
2021-11-28 17:45:57 +0000

    Service Section

--- src/Sections/Services/index.js
+++ src/Sections/Services/index.js
@@ -1,7 +1,176 @@
-// This is Services component
+import styled from "styled-components";
+import TextBlock from "../../components/TextBlock";
+import SvgBlock from "../../components/SvgBlock";
+import Tube from "../../assets/3dtube.png";
+import Cone from "../../assets/3dtriangle.png";
+import Capsule from "../../assets/3dcapsule.png";
+
+// const TextBlock = lazy(() => import("../../components/TextBlock"));
+// const SvgBlock = lazy(() => import("../../components/SvgBlock"));
+
+const ServiceSection = styled.section`
+  width: 100vw;
+  /*background-color: #0a0b10; */
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  padding-top: 20rem;
+`;
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 85vh;
+  z-index: -1;
+  background-color: #0a0b10;
+  background-size: auto 100vh;
+  background-repeat: no-repeat;
+`;
+
+const Title = styled.h1`
+  color: var(--white);
+  display: inline-block;
+  font-size: 2rem;
+  /* margin-top: 4rem; */
+  margin-top: 1rem;
+  position: relative;
+  &::before {
+    content: "";
+    height: 1px;
+    width: 50%;
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0.5rem);
+    /* or 100px */
+    border-bottom: 2px solid var(--pink);
+  }
+`;
+
+const Line = styled.span`
+  border-left: 4px solid var(--background);
+  height: 15rem;
+  margin-top: 2rem;
+  border-radius: 20px 20px 0 0;
+`;
+
+const Triangle = styled.span`
+  width: 0;
+  height: 0;
+  border-left: 1.2rem solid transparent;
+  border-right: 1.2rem solid transparent;
+  border-top: 2rem solid var(--background);
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10rem 10rem;
+  /* margin-bottom: 10rem; */
+  position: relative;
+  @media only Screen and (max-width: 64em) {
+    margin: 10rem calc(4rem + 5vw);
+  }
+  @media only Screen and (max-width: 48em) {
+    display: block;
+    &:last-child {
+      margin-bottom: 2rem;
+    }
+  }
+  @media only Screen and (max-width: 40em) {
+    margin: 10rem calc(2rem + 3vw);
+    &:last-child {
+      margin-bottom: 1rem;
+    }
+  }
+`;
+
+const OBJ = styled.div`
+  position: absolute;
+  top: 80%;
+  right: 35%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20vw;
+  /* z-index: 1; */
+  @media only Screen and (max-width: 48em) {
+    opacity: 0.5;
+  }
+`;
+
+
+ 
 
 const Services = () => {
-  return <div>Services Component</div>;
+  return (
+    <ServiceSection id="services">
+      <Background >
+        <Title className="title">What We Do</Title>
+        <Line id="line" />
+        <Triangle id="triangle" />
+      </Background>
+
+      <Content >
+        <TextBlock
+          topic="Design"
+          title={<h1>We build award winning Designs</h1>}
+          subText={
+            <h5>
+              We help clients to build great design to attract more customers
+            </h5>
+          }
+        />
+        <OBJ>
+          <img src={Tube} alt="Tube Object" width="400" height="400" />
+        </OBJ>
+        <SvgBlock svg="Design.svg" />
+      </Content>
+      <Content >
+        <TextBlock
+          topic="Develop"
+          title={<h1>We Develope high quality Web & App</h1>}
+          subText={
+            <h5>
+              We build appropriate solution to develope your website & app with
+              best tools available
+            </h5>
+          }
+        />
+        <OBJ>
+          <img src={Cone} alt="Cone Object" width="400" height="400" />
+        </OBJ>
+        <SvgBlock svg="Develope.svg" />
+      </Content>
+      <Content>
+        <TextBlock
+          topic="Support"
+          title={<h1>We provide support for your digital presence</h1>}
+          subText={
+            <h5>
+              Once your system is online, we will stay on hand to help you use
+              it and provide technical support and maintenance <br /> your
+              business
+            </h5>
+          }
+        />
+        <OBJ>
+          <img src={Capsule} alt="Capsule Object" width="400" height="400" />
+        </OBJ>
+        <SvgBlock svg="Support.svg" />
+      </Content>
+    </ServiceSection>
+  );
 };
 
 export default Services;
